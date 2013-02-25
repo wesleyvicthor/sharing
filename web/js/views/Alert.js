a46524744cdfbfb333b3707906f1cc39042bc1af
Wesley Victhor Mendes <w.v.mendes.s@gmail.com>
2013-02-25 02:32:00 -0300

    Fixes the loss of events on creating new alerts views.

--- web/js/views/Alert.js
+++ web/js/views/Alert.js
@@ -1,17 +1,14 @@
 define(['backbone', 'underscore', 'jquery'], function (Backbone, _, $) {
     return Backbone.View.extend({
         el: $('<div class="alert hidden"/>'),
-        template: _.template($('script#alert-template').html()),
         events: {
             'click button': 'closeAlert'
         },
 
         render: function () {
-            if ($('.alert-wrapper').children().length) {
-                return;
-            }
-            this.$el.html(this.template(this.options));
+            this.$el.html(_.template($('script#alert-template').html())(this.options));
             $('.alert-wrapper').html(this.$el.fadeIn('fast'));
+            this.delegateEvents();
             return this;
         },
 
